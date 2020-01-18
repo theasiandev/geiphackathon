486c51c90524348fe45d5aa4a1ac7b18828295b4
Bryan Sheng <bryanzhi123@hotmail.com>
2020-01-18 16:01:35 +0800

    Fixed landing page

--- src/LandingLayout.jsx
+++ src/LandingLayout.jsx
@@ -18,8 +18,10 @@ class LandingLayout extends React.Component {
 
         return (
             <div className={styles.backgroundImage}>
-                <div>
-                    <h1 className={styles.pagetitle}>Wander</h1>
+                <div className="titleDivBox">
+                    <div className={styles.pagetitle}>
+                    <h1>Wander</h1>
+                    </div>
                     { redirectElement }
                 </div>
                 <div>
